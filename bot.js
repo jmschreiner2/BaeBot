6e7184e24b38c99a44e1fe3e65f852433ee62ae5
Jacob Schreiner <jmschreiner2@gmail.com>
2018-04-27 07:06:06 -0400

    Removed docker-compose. included scathach command. Safebooru not in nsfw

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const auth = require("./auth.json");
 const getRandomInt = require('./common').getRandomInt;
-const nsfw = [require("./connectors/rule34"), require('./connectors/danborru'), require('./connectors/gelbooru'), require('./connectors/safebooru')];
+const nsfw = [require("./connectors/rule34"), require('./connectors/danborru'), require('./connectors/gelbooru')];
 const sfw = [require('./connectors/safebooru')]
 const real = [require('./connectors/realbooru')]
 const cosplay = [...real, ...nsfw]
@@ -92,6 +92,11 @@ client.on("message", (message) => {
         {
             var params = message.content.split(' ');
             message.channel.send(apis[apiNumber].getPictureUrl(params[1]));
+        }
+
+        if(message.content.startsWith('!scathach'))
+        {
+            message.channel.send(apis[apiNumber].getPictureUrl('scathach_(fate/grand_order)'));
         }
 
     }
